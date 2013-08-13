1fdb2019cbdf8bb76c40f111fbc3681183ec9e38
felixzhao <felixz2010@gmail.com>
2013-08-13 17:25:11 +0800

    add function which can update test vector by match word vector.

---
+++ updatetestvector.py
@@ -0,0 +1,24 @@
+def updatevector(match_word_test, match_word_train, test_vectors, train_vecotrs):
+  updated_vectors = test_vectors.copy()
+  updated_vectors[match_word_train] = train_vecotrs[match_word_train]
+  del updated_vectors[match_word_test]
+  return updated_vectors
+
+if __name__ == '__main__':
+  # define
+  org_word = 'org'
+  rpl_word = 'rpl'
+  org_vec = {}
+  org_vec['org'] = [1.1,1.2,1.3]
+  org_vec['word'] = [2.1,2.2,2.3]
+  rpl_vec = {}
+  rpl_vec['rpl'] = [3.1,3.2,3.3]
+  exp_vec = {}
+  exp_vec['rpl'] = [3.1,3.2,3.3]
+  exp_vec['word'] = [2.1,2.2,2.3]
+  # call function
+  act_vec = updatevector(org_word, rpl_word, org_vec, rpl_vec)
+  # validation
+  print( act_vec == exp_vec)
+  for k in act_vec.keys():
+    print( k + str(act_vec[k]))
